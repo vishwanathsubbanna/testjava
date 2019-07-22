ba42fb3ee39a3f92fdf98d918964c0c4583312c2
Administrator <Administrator@192.168.1.11>
2019-07-22 12:53:30 +0530

    Added print code

--- src/testjava/TestJava.java
+++ src/testjava/TestJava.java
@@ -16,6 +16,7 @@ public class TestJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Welcome to JAVA");
     }
     
 }
